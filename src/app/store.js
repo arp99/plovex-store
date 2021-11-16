8a3a6d99d73614b96485e58d11a9ea2ac6718f56
Arpan Mondal <arpan19991025@gmail.com>
2021-11-17 01:06:14 +0530

    Add products, wishlist reducers to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit"
 import authReducer from "./Features/Auth/auth"
 import homepageReducer from "./Features/HomePage/homePage"
+import productReducer from "./Features/Products/products"
+import wishlistReducer from "./Features/Wishlist/wishlist"
 
 export const store  = configureStore({
     reducer : {
         auth : authReducer,
         homePage : homepageReducer,
+        product : productReducer,
+        wishlist : wishlistReducer 
     }
 })
