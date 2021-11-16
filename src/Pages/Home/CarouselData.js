62ac54479095bf5ab327f3bf7d854cb825dec879
Arpan Mondal <arpan19991025@gmail.com>
2021-11-17 01:18:43 +0530

    Pass routes in carousel data

--- src/Pages/Home/CarouselData.js
+++ src/Pages/Home/CarouselData.js
@@ -1,18 +1,22 @@
 export const CarouselData = [
     {
-        url : "https://cdn.shopify.com/s/files/1/0536/3594/0515/files/1800x800-banner-bsc1_9e891a9a-d2d4-4f62-b5e1-73d1d69e4732_1800x.jpg?v=1632310265",
-        title : "Classic Elegant Shirts"
+        url : "https://res.cloudinary.com/arp99/image/upload/v1637074881/plovex-store/slide1_c0q0yt.jpg",
+        title : "Classic Elegant Shirts",
+        route : "/products/brand/indie"
     },
     {
-        url : "//cdn.shopify.com/s/files/1/0536/3594/0515/files/tess-re_1800x.jpg?v=1635880907",
-        title : "Cool t-Shirts"
+        url : "https://res.cloudinary.com/arp99/image/upload/v1637074881/plovex-store/slide2_qb4v9e.jpg",
+        title : "Cool t-Shirts",
+        route : "/products/brand/cityof_"
     },
     {
-        url : "//cdn.shopify.com/s/files/1/0536/3594/0515/files/Homepage-banner-2re_8a8cec60-6117-4717-b8cb-ee6155404366_1800x.jpg?v=1633149451",
-        title : "Jeans That actually Fit"
+        url : "https://res.cloudinary.com/arp99/image/upload/v1637074881/plovex-store/slide3_n9wmmt.jpg",
+        title : "Jeans That actually Fit",
+        route : "/products/brand/korra"
     },
     {
-        url : "//cdn.shopify.com/s/files/1/0536/3594/0515/files/A._Dresses_1800x800_8b5e493d-805d-41be-a407-65352c226b7e_1800x.jpg?v=1633088173",
-        title : "Easy, Effortless Clothes"
+        url : "https://res.cloudinary.com/arp99/image/upload/v1637074881/plovex-store/slide4_deabt5.jpg",
+        title : "Easy, Effortless Clothes",
+        route : "/products/brand/flora"
     },
 ]
