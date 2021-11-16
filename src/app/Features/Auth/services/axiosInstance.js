d0d7e5b571e4e2ea77f97d8fe5406f0e66941c9f
Arpan Mondal <arpan19991025@gmail.com>
2021-11-17 01:01:28 +0530

    Create axios instance and interceptors

---
+++ src/app/Features/Auth/services/axiosInstance.js
@@ -0,0 +1,25 @@
+import axios from "axios"
+
+const axiosInstance = axios.create({
+    baseURL : `https://plovex-store-backend.herokuapp.com/`
+})
+
+
+//axios instance for requests which require authorization by token
+export const axiosAuthorization = axios.create({
+    baseURL : `https://plovex-store-backend.herokuapp.com/`
+})
+
+axiosAuthorization.interceptors.request.use(config => {
+    const token = localStorage.getItem('token') ?? null
+    // const { token } = useSelector( state => state.auth )
+    if( token ){
+        config.headers["authorization"] = `Bearer ${token}` 
+    }
+    console.log("Token inside axiosInstance config: ", { token })
+    console.log("axios interceptor config: ", config)
+    return config
+})
+
+
+export default axiosInstance
