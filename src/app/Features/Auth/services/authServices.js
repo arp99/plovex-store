a7fb19ef3fee98857cd1b40e66c18c3dca4cfde6
Arpan Mondal <arpan19991025@gmail.com>
2021-11-17 01:02:04 +0530

    Use axios instance

--- src/app/Features/Auth/services/authServices.js
+++ src/app/Features/Auth/services/authServices.js
@@ -1,14 +1,14 @@
-import axios from "axios"
+import axiosInstance from "./axiosInstance"
 
 export const loginUser = async ( email, password ) => {
-    return axios.post("url",{
+    return axiosInstance.post("login",{
         email,
         password
     })
 }
 
 export const signupUser = async ( firstName, lastName, email, password ) => {
-    return axios.post("url",{
+    return axiosInstance.post("signup",{
         firstName,
         lastName,
         email,
