4670197fb7e7b240ef74d1046c57d6ed99b7d6fd
Arpan Mondal <arpan19991025@gmail.com>
2021-11-18 02:34:30 +0530

    Export productDescription page

--- src/Pages/Products/index.js
+++ src/Pages/Products/index.js
@@ -1,4 +1,4 @@
 export { Brand } from "./Brands"
 export { Category } from "./Category"
 export { NewReleased } from "./NewReleased"
-export { Products } from "./Products"
+export { ProductDescription } from "./ProductDescription"
