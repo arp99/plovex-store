b2a3470e98c0e833f0995e1ac712b0ce052c7347
Arpan Mondal <arpan19991025@gmail.com>
2021-11-18 02:45:15 +0530

    Add one styled component

--- src/Components/styledComponents/StyledComponents.jsx
+++ src/Components/styledComponents/StyledComponents.jsx
@@ -6,3 +6,9 @@ export const StyledGrid = styled.div`
     max-height:max-content;
     ${tw`w-full grid grid-cols-2 gap-4 mt-6 md:grid-cols-4`}
 `
+export const IncrementDecrementBtn = styled.button`
+    ${tw`px-4 text-xl 
+        bottom-1 border-solid border-primary-ligter rounded-md bg-transparent 
+        hover:(text-secondary bg-primary-ligter)`
+    }
+`
