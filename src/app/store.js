d49c8bb1d19d85804fc2bf396230ee07aa7cc589
Arpan Mondal <arpan19991025@gmail.com>
2021-11-18 02:47:52 +0530

    Add cartreducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -3,12 +3,14 @@ import authReducer from "./Features/Auth/auth"
 import homepageReducer from "./Features/HomePage/homePage"
 import productReducer from "./Features/Products/products"
 import wishlistReducer from "./Features/Wishlist/wishlist"
+import cartReducer from "./Features/Cart/Cart"
 
 export const store  = configureStore({
     reducer : {
         auth : authReducer,
         homePage : homepageReducer,
         product : productReducer,
-        wishlist : wishlistReducer 
+        wishlist : wishlistReducer,
+        cart : cartReducer
     }
 })
