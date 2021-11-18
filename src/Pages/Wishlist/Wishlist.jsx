9ede8250e016024ebf2c54481d079315d01d7489
Arpan Mondal <arpan19991025@gmail.com>
2021-11-18 18:35:40 +0530

    Styles: Update UI

--- src/Pages/Wishlist/Wishlist.jsx
+++ src/Pages/Wishlist/Wishlist.jsx
@@ -3,14 +3,21 @@ import "twin.macro"
 import { useSelector } from "react-redux"
 import EmptyWishlist from "./assets/nothing_found.png"
 import { Link } from "react-router-dom"
-import { Button } from "../../Components"
+import { Button, Productcard, SkeletonUI, StyledGrid } from "../../Components"
 
 export const Wishlist = () => {
-    const { products } = useSelector( state => state.wishlist)
-    return(
-        <main tw="w-full min-h-screen pt-24">
+    const { products, wishlistFetchStatus } = useSelector( state => state.wishlist)
+    console.log({ wishlistFetchStatus })
+    return(               
+        <main tw="pt-24 min-h-screen text-center mx-auto w-11/12" >
             {
-                products.length === 0 &&
+                wishlistFetchStatus === 'loading' && 
+                <StyledGrid>
+                    <SkeletonUI />
+                </StyledGrid>
+            }
+            {
+                products.length === 0 && wishlistFetchStatus === 'fulfilled' &&
                 <div tw="w-max flex flex-col items-center mx-auto">
                     <img 
                         src={EmptyWishlist} 
@@ -23,6 +30,30 @@ export const Wishlist = () => {
                     </Link>
                 </div>
             
+            }
+            {
+                products.length > 0 && 
+                <>
+                    <h1>Wishlist Items</h1>
+                    <StyledGrid>
+                        {
+                            products.map(({ _id, image, name, price, oldPrice, newLaunch })=>{
+                                return(
+                                    <Productcard 
+                                        key = { _id }
+                                        productId = { _id }
+                                        productImg = { image }
+                                        title = { name }
+                                        newPrice = { price }
+                                        oldPrice = { oldPrice }
+                                        newProduct = { newLaunch }
+                                        inWishlist = { true }
+                                    />
+                                )
+                            })
+                        }
+                    </StyledGrid>
+                </>
             }
         </main>
     )
