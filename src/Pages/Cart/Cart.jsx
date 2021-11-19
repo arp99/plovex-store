4b7b8bdeca96a92407fca4ce87fd7472a19d9bfa
Arpan Mondal <arpan19991025@gmail.com>
2021-11-19 15:52:44 +0530

    Modify cart page

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -4,13 +4,20 @@ import { Button } from "../../Components"
 import { useSelector } from "react-redux"
 import EmptyCart from "./assets/nothing_found.png"
 import { Link } from "react-router-dom"
+import { css } from "@emotion/react"
+import tw from "twin.macro"
+import { CartCard } from "./Components/CartCard"
+import { PriceDetails } from "./Components/PriceDetails"
+import { getBill } from "../../app/Features/Cart/services/cartServices"
 
 export const Cart = () => {
-    const { products } = useSelector( state => state.wishlist)
+    const { products, cartFetchStatus } = useSelector( state => state.cart )
+    const { userId } = useSelector( state => state.auth )
+    console.log(products)
     return(
-        <main tw="w-full min-h-screen pt-24">
+        <main tw="w-full min-h-screen pt-28 flex flex-col justify-center items-center md:(flex-row items-start)">
             {
-                products.length === 0 &&
+                products.length === 0 && cartFetchStatus === 'fulfilled' &&
                 <div tw="w-max flex flex-col items-center mx-auto">
                     <img 
                         src={EmptyCart} 
@@ -24,7 +31,45 @@ export const Cart = () => {
                         <Button variant="secondary">Shop Now</Button>
                     </Link>
                 </div>
-            
+            }
+            {
+                products.length > 0 && 
+                <>
+                    <div
+                        css={
+                            css`
+                                max-width:700px;
+                                max-height:600px;
+                                ${tw`w-4/5 h-full overflow-y-scroll shadow-xl relative`}
+                            `
+                        }
+                    >
+                        {
+                            products.map(({ product, quantity }) => (
+                                <CartCard 
+                                    key={product._id}
+                                    product={product}
+                                    quantity={quantity}
+                                    userId={userId}
+                                />
+                            ))
+                        }
+                        <div
+                            css={
+                                css`
+                                    box-shadow: 0 -2px 15px -2px #6b6969;
+                                    ${tw`w-full sticky bottom-0 text-right py-2 bg-secondary`}
+                                `
+                            }
+                        >
+                            <Button variant="primary">Place Order</Button>
+                        </div>
+                    </div>
+                    <PriceDetails 
+                        itemQuantity={ products.length } 
+                        bill={ getBill(products) }
+                    />
+                </>
             }
         </main>
     )
